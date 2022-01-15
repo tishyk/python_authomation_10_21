2208e561253006a72a5cda928239bdc0898a518f
Nikita Limarchuk <nikita.limarchuk@gmail.com>
2022-01-15 15:28:33 +0200

    add examples for lesson_22

---
+++ lesson_22/README.md
@@ -0,0 +1,5 @@
+## Unit test Mocks, Selenium locators, driver
+
+1. Unit test Mocks for mocking dependencies
+2. Locators (CSS, XPATH)
+3. Selenium Webdriver basics
---
+++ lesson_22/main.py
@@ -0,0 +1,33 @@
+from time import sleep
+
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+
+
+driver = Chrome("./chromedriver")
+driver.get("https://www.w3schools.com/cssref/css_selectors.asp")
+# maximize window size up to screen size on device
+driver.maximize_window()
+
+element: WebElement = driver.find_element(By.XPATH, "//a[contains(text(), 'Next')]")
+# sleep(1)
+# element.click()
+# click on search icon
+search_button: WebElement = driver.find_element(By.XPATH, "//a[@title='Search W3Schools']")
+search_button.click()
+sleep(1)
+search_bar: WebElement = driver.find_element(By.XPATH, "//input[@id='gsc-i-id1']")
+search_bar.send_keys("Hello WOrld=)")
+sleep(3)
+search_bar.clear()
+sleep(3)
+
+#save button screenshot in binary mod into the png container
+with open("next_button.png", "wb") as file:
+    file.write(element.screenshot_as_png)
+
+driver.save_screenshot("./screenshot.png")
+
+print(driver.current_url)
+driver.quit()
